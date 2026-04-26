9d6b30bde729b1bdf8be4bb6175a831c5ea9a274
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-26 17:29:11 +0200

    question 1,

--- object.js
+++ object.js
@@ -1,1 +1,7 @@
-console.log("Hello!");
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = "Double Cheese Burger";
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // 'Double Cheese Burger'
+console.log(secondBurger.name); // 'Double Cheese Burger'
