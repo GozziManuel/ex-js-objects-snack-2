60692991de16e1bda38e33757fa5620749688e2b
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-26 17:32:31 +0200

    question 2

--- object.js
+++
@@ -1,7 +0,0 @@
-const hamburger = { name: "Cheese Burger", weight: 250 };
-const secondBurger = hamburger;
-secondBurger.name = "Double Cheese Burger";
-secondBurger.weight = 500;
-
-console.log(hamburger.name); // 'Double Cheese Burger'
-console.log(secondBurger.name); // 'Double Cheese Burger'
