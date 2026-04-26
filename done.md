db8eaee5a6668f201f4c485162d40368b3538bfd
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-26 17:55:26 +0200

    bonus 1

--- done.md
+++ done.md
@@ -50,3 +50,35 @@ const thirdBurger = structuredClone(hamburger);
 ---
 
 question 4
+const chef = {
+name: "Chef Hyur",
+age: 29,
+makeBurger: (num = 1) => {
+console.log(`Ecco ${num} hamburger per te!`);
+},
+};
+
+const restaurant = {
+name: "Hyur's Burgers",
+address: {
+street: "Main Street",
+number: 123,
+},
+openingDate: new Date(2025, 3, 11),
+isOpen: false,
+};
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+const chefCopy = { ...chef };
+console.log(chefCopy);
+chefCopy.makeBurger();
+//shallow copy con spread op. perché mi permette di copiare anche la funzione dell'oggetto dato che con le deep copy non me lo permetterebbe.
+
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
+const restaurantCopy = structuredClone(restaurant);
+console.log(restaurantCopy);
+// deep copy con structuredClone perché mi permette di copiare Date.now nel modo corretto
+
+---
+
+question 5
--- object.js
+++ object.js
@@ -1,28 +1,47 @@
-const chef = {
-  name: "Chef Hyur",
-  age: 29,
-  makeBurger: (num = 1) => {
-    console.log(`Ecco ${num} hamburger per te!`);
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  maker: {
+    name: "Anonymous Chef",
+    restaurant: {
+      name: "Hyur's Burgers",
+      address: "Main Street, 123",
+      isOpen: true,
+    },
+    age: 29,
   },
 };
 
-const restaurant = {
-  name: "Hyur's Burgers",
-  address: {
-    street: "Main Street",
-    number: 123,
-  },
-  openingDate: new Date(2025, 3, 11),
-  isOpen: false,
-};
+const newRestaurant = { ...hamburger.maker.restaurant };
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+// restaurant: {
+//       name: "Hyur's II",
+//       address: "Second Street, 12",
+//       isOpen: true,
+//     },
+const secondBurger = { ...hamburger };
+// const secondBurger = {
+//   name: "Cheese Burger",
+//   weight: 250,
+//   maker: {
+//     name: "Anonymous Chef",
+//     restaurant: {
+//       name: "Hyur's Burgers",
+//       address: "Main Street, 123",
+//       isOpen: true,
+//     },
+//     age: 29,
+//   },
+// };
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
 
-// Qual è il metodo migliore per clonare l’oggetto chef, e perché?
-const chefCopy = { ...chef };
-console.log(chefCopy);
-chefCopy.makeBurger();
-//shallow copy  con spread op. perché mi permette di copiare anche la funzione dell'oggetto dato che con le deep copy non me lo permetterebbe.
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+console.log(hamburger.maker.name); // "Chef Hyur"
+console.log(secondBurger.maker.name); // "Chef Hyur"
+console.log(hamburger.maker.restaurant.name); // "Hyur's II"
+console.log(secondBurger.maker.restaurant.name); // "Hyur's II"
 
-// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
-const restaurantCopy = structuredClone(restaurant);
-console.log(restaurantCopy);
-// deep copy con structuredClone perché mi permette di copiare Date.now nel modo corretto
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// 5
